bcf0f6bd0f64b07722326c9e8f5794501f3ff94b
Marcelo Braga <mcbraga@hotmail.com>
2017-02-08 10:59:55 -0200

    Add app models.

---
+++ app/Models/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\UserThanks;
+use App\Models\EnterpriseThanks;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'telephone', 'address', 'cpf', 'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Enable soft deletes to the user model.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'expires_at',
+        'deleted_at'
+    ]
+
+    /**
+     * Get the user thanks the user sent
+     * 
+     * @return UserThanks[]
+     */
+    public function sentUserThanks()
+    {
+        return $this->hasMany('UserThanks', 'sender', 'user_id');
+    }
+
+    /**
+     * Get the user thanks the user received
+     * 
+     * @return UserThanks[]
+     */
+    public function receivedUserThanks()
+    {
+        return $this->hasMany('UserThanks', 'receipt', 'user_id');
+    }
+
+    /**
+     * Get the enterprise thanks the user sent
+     * 
+     * @return EnterpriseThanks[]
+     */
+    public function enterpriseThanks()
+    {
+        return $this->hasMany('EnterpriseThanks');
+    }
+}
---
+++ app/Models/UserThanks.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserThanks extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Enable soft deletes to the user thanks model.
+     *
+     * @var array
+     */
+    $protected $dates = [
+    	'expires_at',
+    	'deleted_at',
+    	'date'
+    ]
+
+    /**
+     * Get the user that received the user thanks.
+     * 
+     * @return User
+     */
+    public function receipt()
+    {
+    	return $this->belongsTo('User', 'user_id', 'receipt');
+    }
+
+    /**
+     * Get the user that sent the user thanks.
+     * 
+     * @return User
+     */
+    public function sender()
+    {
+    	return $this->belongsTo('User', 'user_id', 'sender');
+    }
+}
