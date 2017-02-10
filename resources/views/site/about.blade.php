441ea7f4b3c40292904265fec1ad373169a4f617
Marcelo Braga <mcbraga@hotmail.com>
2017-02-10 17:24:02 -0200

    Add about template. Change styles from rules template.

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -3,11 +3,64 @@
 @section('content')
 
 <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="height:400px;">
-                
-            </div>
+	<div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <section class="enterprise">            	
+            	<div class="mission-box">
+            	    <img src="images/mission-bg.png" />
+			    </div>
+			    <div>
+			    	
+			    </div>
+            </section>
         </div>
     </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <section class="">
+            	<h1 class="rules-howto">Nossa equipe</h1>
+        		<div class="col-team">
+			    	<div class="team"> 
+                    	<img src="images/colaborador1.png" alt="Equipe" title="Equipe" />
+                    	<i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+						<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+						<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                    	<div class="team-description">
+                    		<p>Cátia Mota</p>
+                    		<p class="job">Diretora de Marketing</p>
+                    		<p class="job-description">Lorem ipsum dolor sit amet consectetur adipiscing elit in pulvinar amet consectetur amet adipiscing elit.</p>
+						</div>
+                	</div>
+			    </div>
+			    <div class="col-team">
+			      	<div class="team"> 
+                    	<img src="images/colaborador2.png" alt="Equipe" title="Equipe" />
+                    	<i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+						<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+						<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                    	<div class="team-description">
+                    		<p>Vítor Rodrigues</p>
+                    		<p class="job">Diretor</p>
+                    		<p class="job-description">Lorem ipsum dolor sit amet consectetur adipiscing elit in pulvinar amet consectetur amet adipiscing elit.</p>
+						</div>
+                	</div>
+			    </div>
+			    <div class="col-team">
+			      	<div class="team"> 
+                    	<img src="images/colaborador3.png" alt="Equipe" title="Equipe" />
+                    	<i class="fa fa-linkedin fa-2x" aria-hidden="true"></i>
+						<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+						<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                    	<div class="team-description">
+                    		<p>Gustavo Dantas</p>
+                    		<p class="job">Diretor de Tecnologia</p>
+                    		<p class="job-description">Lorem ipsum dolor sit amet consectetur adipiscing elit in pulvinar amet consectetur amet adipiscing elit.</p>
+						</div>
+                	</div>
+			    </div>			    
+            </section>            
+        </div>
+    </div>
+</div>
 
 @endsection
