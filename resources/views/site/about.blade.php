cccbc1669362341996944b7ac80e1b27257bdd19
Marcelo Braga <mcbraga@hotmail.com>
2017-02-10 23:30:33 -0200

    Add about view.

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -6,19 +6,19 @@
 	<div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <section class="enterprise">            	
-            	<div class="mission-box">
+            	<div class="about-image">
             	    <img src="images/mission-bg.png" />
 			    </div>
-			    <div>
-			    	
+			    <div class="about-text">
+			    	<p class="job">Lorem ipsum dolor sit amet consectetur adipiscing elit in pulvinar amet consectetur amet adipiscing elit.</p>
 			    </div>
             </section>
         </div>
     </div>
-    <div class="row">
+    <div class="row team-background">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <section class="">
-            	<h1 class="rules-howto">Nossa equipe</h1>
+            <section class="about-team">
+            	<h1 class="team-title">Nossa equipe</h1>
         		<div class="col-team">
 			    	<div class="team"> 
                     	<img src="images/colaborador1.png" alt="Equipe" title="Equipe" />
@@ -26,7 +26,7 @@
 						<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
 						<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
                     	<div class="team-description">
-                    		<p>Cátia Mota</p>
+                    		<p class="name">Cátia Mota</p>
                     		<p class="job">Diretora de Marketing</p>
                     		<p class="job-description">Lorem ipsum dolor sit amet consectetur adipiscing elit in pulvinar amet consectetur amet adipiscing elit.</p>
 						</div>
@@ -39,7 +39,7 @@
 						<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
 						<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
                     	<div class="team-description">
-                    		<p>Vítor Rodrigues</p>
+                    		<p class="name">Vítor Rodrigues</p>
                     		<p class="job">Diretor</p>
                     		<p class="job-description">Lorem ipsum dolor sit amet consectetur adipiscing elit in pulvinar amet consectetur amet adipiscing elit.</p>
 						</div>
@@ -52,7 +52,7 @@
 						<i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
 						<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
                     	<div class="team-description">
-                    		<p>Gustavo Dantas</p>
+                    		<p class="name">Gustavo Dantas</p>
                     		<p class="job">Diretor de Tecnologia</p>
                     		<p class="job-description">Lorem ipsum dolor sit amet consectetur adipiscing elit in pulvinar amet consectetur amet adipiscing elit.</p>
 						</div>
