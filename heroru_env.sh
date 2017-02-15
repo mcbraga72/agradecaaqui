9dbe3d1c1e6974613dd2716f1885679ffd1031f1
Marcelo Braga <mcbraga@hotmail.com>
2017-02-15 21:46:16 -0200

    Change enviroment variables on Heroku app.

--- heroru_env.sh
+++ heroru_env.sh
@@ -2,7 +2,7 @@ heroku config:set APP_ENV=local
 heroku config:set APP_KEY=base64:wIZx+Cd9mNwszMUL1+H3CsfAnlXNQyhj7OC8ekKbZZw=
 heroku config:set APP_DEBUG=true
 heroku config:set APP_LOG_LEVEL=debug
-heroku config:set APP_URL=https://stark-wildwood-60445.herokuapp.com
+heroku config:set APP_URL=https://agradecaaqui.herokuapp.com
 
 heroku config:set DB_CONNECTION=pgsql
 heroku config:set DB_HOST=ec2-54-163-254-48.compute-1.amazonaws.com
@@ -16,7 +16,7 @@ heroku config:set CACHE_DRIVER=file
 heroku config:set QUEUE_DRIVER=sync
 
 heroku config:set SESSION_DRIVER=file
-heroku config:set SESSION_DOMAIN=stark-wildwood-60445.herokuapp.com
+heroku config:set SESSION_DOMAIN=agradecaaqui.herokuapp.com
 
 heroku config:set REDIS_HOST=127.0.0.1
 heroku config:set REDIS_PASSWORD=null
