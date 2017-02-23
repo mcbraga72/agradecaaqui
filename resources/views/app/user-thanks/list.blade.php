467de63fa790f720928dca23d668eebad8951852
Marcelo Braga <mcbraga@hotmail.com>
2017-02-23 19:15:44 -0300

    Change layout for user and enterprise thanks list.

--- resources/views/app/user-thanks/list.blade.php
+++ resources/views/app/user-thanks/list.blade.php
@@ -3,9 +3,34 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
+        <div class="col-xs-12 col-xs-offset-0 col-sm-12 col-sm-offset-0 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 home">
+            <img class="logo-login" src="{{asset('images/logo.png')}}"" />
+            <h1 class="support">Comentários</h1>            
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('busca') }}">
+            {{ csrf_field() }}
+                <div class="form-group{{ $errors->has('busca') ? ' has-error' : '' }}">
+                    <br><br>
+                    <div class="col-md-6">
+                        <input id="busca" type="busca" class="form-control search" name="busca" value="{{ old('busca') }}" placeholder="Pesquisar por" required autofocus>
+                        @if ($errors->has('busca'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('busca') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4">
+                        <button type="submit" class="btn pink-button">Pesquisar</button>                        
+                    </div>
+                </div>
+            </form>
+        </div>          
+    </div>    
+    <div class="row">
         <div class="col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
-            <a href="{{ url('app/agradecimento-usuario/criar') }}" class="btn btn-success btn-add"><i class="fa fa-plus" aria-hidden="true"></i>&nbsp;Cadastrar agradecimento - Usuário</a>
-            <div class="panel panel-default">                
+            <a href="{{ url('app/agradecimento-usuario/criar') }}" class="btn btn-success btn-pink"><i class="fa fa-plus" aria-hidden="true"></i>&nbsp;Cadastrar agradecimento para uma pessoa</a>
+            <div class="panel panel-default" style="margin-top: 20px !important">                
                 <!-- Lista de usuários -->
                 <div class="table-responsive">          
                     <table class="table">
