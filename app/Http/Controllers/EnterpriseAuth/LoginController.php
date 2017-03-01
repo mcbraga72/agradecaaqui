64614301e788d52df037e38ef56a5b6928ec1019
Marcelo Braga <mcbraga@hotmail.com>
2017-03-01 11:14:35 -0300

    Correct errors on enterprise form login.

--- app/Http/Controllers/EnterpriseAuth/LoginController.php
+++ app/Http/Controllers/EnterpriseAuth/LoginController.php
@@ -47,7 +47,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('enterprise.login');
+        return view('enterprise-auth.login');
     }
 
     /**
