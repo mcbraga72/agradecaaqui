c0e2cbbc8bba8d53e7e37b9288906c31ac34392c
Marcelo Braga <mcbraga@hotmail.com>
2017-03-14 10:07:28 -0300

    Add content removed accidentally.

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -166,6 +166,12 @@
     <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 	<script type="text/javascript">
+		$(document).ready(function() {
+    		$('#enterpriseThanks').show();
+	    	$('#userThanks').hide();
+	    	$('#enterprisesButton').addClass('button-selected');
+	    });	
+
 		$('#peopleButton').click(function(){
 	    	$('#enterpriseThanks').hide();
 	    	$('#userThanks').show();
