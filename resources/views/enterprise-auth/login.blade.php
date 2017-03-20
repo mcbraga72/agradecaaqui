35dc973ea3ef63a54ff206900489e711a9acb6c4
Marcelo Braga <mcbraga@hotmail.com>
2017-03-20 16:05:19 -0300

    Inclusão de relatórios (agradecimentos por cidade e por sexo). Remoção de comentários desnecessários na view layout (enterprise area). Resolução de problemas com token na view de autenticação da área da empresa.

--- resources/views/enterprise-auth/login.blade.php
+++ resources/views/enterprise-auth/login.blade.php
@@ -4,46 +4,28 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>Agradeça Aqui - Área Administrativa</title>
-
-    <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
-
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
-                    <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/empresa/painel') }}">Agradeça Aqui - Área da Empresa</a>
                 </div>
-
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
@@ -69,7 +51,6 @@
                             </li>
                         @endif
                     </ul>
-
                     {{--<!-- Right Side Of Navbar Enterprise -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
@@ -98,7 +79,6 @@
                             <li><a href="{{ route('empresa/cadastro') }}">Cadastro</a></li>
                         @endif
                     </ul>--}}
-
                 </div>
             </div>
         </nav>
@@ -110,13 +90,10 @@
                         <div class="panel-body">
                             <form class="form-horizontal" role="form" method="POST" action="{{ url('/empresa/entrar') }}">
                                 {{ csrf_field() }}
-
                                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                     <label for="email" class="col-md-4 control-label">E-Mail</label>
-
                                     <div class="col-md-6">
                                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
                                         @if ($errors->has('email'))
                                             <span class="help-block">
                                                 <strong>{{ $errors->first('email') }}</strong>
@@ -124,13 +101,10 @@
                                         @endif
                                     </div>
                                 </div>
-
                                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                     <label for="password" class="col-md-4 control-label">Senha</label>
-
                                     <div class="col-md-6">
                                         <input id="password" type="password" class="form-control" name="password" required>
-
                                         @if ($errors->has('password'))
                                             <span class="help-block">
                                                 <strong>{{ $errors->first('password') }}</strong>
@@ -138,7 +112,6 @@
                                         @endif
                                     </div>
                                 </div>
-
                                 <div class="form-group">
                                     <div class="col-md-6 col-md-offset-4">
                                         <div class="checkbox">
@@ -148,13 +121,11 @@
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="form-group">
                                     <div class="col-md-8 col-md-offset-4">
                                         <button type="submit" class="btn btn-primary">
                                             Login
                                         </button>
-
                                         <a class="btn btn-link" href="{{ url('/empresa/trocar-senha') }}">Esqueceu sua senha?</a>
                                     </div>
                                 </div>
@@ -165,8 +136,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Scripts -->
     <script src="/js/app.js"></script>
 </body>
 </html>
