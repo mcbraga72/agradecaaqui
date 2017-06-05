791f74f3ee2248d35933bb9f0a5de48d913d8d1c
Marcelo Braga <mcbraga@hotmail.com>
2017-06-05 17:23:47 -0300

    Import Mail class in controller.

--- app/Http/Controllers/EnterpriseAdminController.php
+++ app/Http/Controllers/EnterpriseAdminController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\EnterpriseRequest;
 use App\Models\Category;
 use App\Models\Enterprise;
 use Illuminate\Http\Request;
+use Mail;
 
 class EnterpriseAdminController extends Controller
 {
