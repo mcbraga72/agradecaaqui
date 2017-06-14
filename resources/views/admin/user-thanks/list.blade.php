c27f57f124827eefd1aea25a08eb855ea842352d
Marcelo Braga <mcbraga@hotmail.com>
2017-06-14 17:56:04 -0300

    Correct errors on item menu ('agradecimentos'), because it's not expanding.

--- resources/views/admin/user-thanks/list.blade.php
+++ resources/views/admin/user-thanks/list.blade.php
@@ -129,8 +129,6 @@
         </div>
     </div>
     <link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/vue.resource/0.9.3/vue-resource.min.js"></script>
