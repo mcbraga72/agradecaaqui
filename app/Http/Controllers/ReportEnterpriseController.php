71634299d9528de431f76df58f3c6b936c3a97e9
Marcelo Braga <mcbraga@hotmail.com>
2017-07-13 22:48:00 -0300

    Correct errors on enterprise reports.

--- app/Http/Controllers/ReportEnterpriseController.php
+++ app/Http/Controllers/ReportEnterpriseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Enterprise;
 use App\Models\EnterpriseThanks;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReportEnterpriseController extends Controller
@@ -19,28 +20,28 @@ class ReportEnterpriseController extends Controller
 		return view('enterprise.report.index');
 	}
 
-    /**
-	 * Generate reports by state
-	 *
-	 * @return Response
-	 */
-	public function generateStateReport()
-	{
-		$stateReport = User::select('state', \DB::raw("count(users.state) as thanks"))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.state')->get();
-		
-		return $stateReport;
-	}
-
 	/**
 	 * Generate reports by city
 	 *
 	 * @return Response
 	 */
-	public function generateCityReport()
+	public function generateCityReport($start, $end)
 	{
-		$cityReport = User::select('city', \DB::raw("count(users.city) as thanks"))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.city')->get();
+		$cityReport = User::select('city', \DB::raw("count(users.city) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.city')->get();
 		
 		return $cityReport;
+	}
+
+    /**
+	 * Generate reports by state
+	 *
+	 * @return Response
+	 */
+	public function generateStateReport($start, $end)
+	{
+		$stateReport = User::select('state', \DB::raw("count(users.state) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.state')->get();
+		
+		return $stateReport;
 	}
 
 	/**
@@ -48,23 +49,95 @@ class ReportEnterpriseController extends Controller
 	 *
 	 * @return Response
 	 */
-	public function generateGenderReport()
+	public function generateGenderReport($start, $end)
 	{
-		$genderReport = User::select('gender', \DB::raw("count(users.gender) as thanks"))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.gender')->get();
+		$genderReport = User::select('gender', \DB::raw("count(users.gender) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.gender')->get();
 		
 		return $genderReport;
 	}
 
 	/**
+	 * Generate reports by marital status
+	 *
+	 * @return Response
+	 */
+	public function generateMaritalStatusReport($start, $end)
+	{
+		$maritalStatusReport = User::select('maritalStatus', \DB::raw("count(users.maritalStatus) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.maritalStatus')->get();
+		
+		return $maritalStatusReport;
+	}
+
+	/**
+	 * Generate reports by religion
+	 *
+	 * @return Response
+	 */
+	public function generateReligionReport($start, $end)
+	{
+		$religionReport = User::select('religion', \DB::raw("count(users.religion) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.religion')->get();
+		
+		return $religionReport;
+	}
+
+	/**
+	 * Generate reports by education
+	 *
+	 * @return Response
+	 */
+	public function generateEducationReport($start, $end)
+	{
+		$educationReport = User::select('education', \DB::raw("count(users.education) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.education')->get();
+		
+		return $educationReport;
+	}
+
+	/**
+	 * Generate reports by profession
+	 *
+	 * @return Response
+	 */
+	public function generateProfessionReport($start, $end)
+	{
+		$professionReport = User::select('profession', \DB::raw("count(users.profession) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.profession')->get();
+		
+		return $professionReport;
+	}
+
+	/**
+	 * Generate reports by income
+	 *
+	 * @return Response
+	 */
+	public function generateIncomeReport($start, $end)
+	{
+		$incomeReport = User::select('income', \DB::raw("count(users.income) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.income')->get();
+		
+		return $incomeReport;
+	}
+
+	/**
+	 * Generate reports by soccer team
+	 *
+	 * @return Response
+	 */
+	public function generateSoccerTeamReport($start, $end)
+	{
+		$soccerTeamReport = User::select('soccerTeam', \DB::raw("count(users.soccerTeam) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.soccerTeam')->get();
+		
+		return $soccerTeamReport;
+	}
+	
+	/**
 	 * Generate a custom report
 	 *
 	 * @return Response
 	 */
-	public function generateCustomReport($type, $start='2017-06-01', $end='2017-07-10')
+	public function generateCustomReport($type, $start, $end)
 	{		
-		$customReport = User::select('users.' . $type, \DB::raw("count(users.gender) as thanks"))->whereBetween('thanksDateTime', array('2017-06-01', '2017-07-10'))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.' . $type)->toSql();
+		$customReport = User::select('users.' . $type, \DB::raw("count(users.$type) as thanks"))->whereBetween('thanksDateTime', array(new Carbon($start), new Carbon($end)))->join('enterprise_thanks', 'enterprise_thanks.user_id', '=', 'users.id')->groupBy('users.' . $type)->get();
 		
-		dd($customReport);
-		return response()->json($customReport);		
+		//dd($customReport);
+		return $customReport;
 	}
 }
