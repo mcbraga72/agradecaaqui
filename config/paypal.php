a31e054e68194ad07d4e1f2b1b446d2c638d0bbb
Marcelo Braga <mcbraga@hotmail.com>
2017-07-24 22:26:08 -0300

    Add Paypal credentials.

--- config/paypal.php
+++ config/paypal.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'client_id' =>'agradecaaquicontato@gmail.com',
-	'secret' => 'agradeca2017',
+	'client_id' =>'AVc4QxpdLph6UjkfvL3q15jF1IBxQBd-ZlGAFbTwhw1NIvDuRYkRwgMw9UXpEQg7I0owNNZn6UEeycZP',
+	'secret' => 'EN7qf2mvI_f8isRvCuiYVVg8hRa5w1KXoDVPEXOYclWYZ0dwAtELdmRgs28sS_dFDpPPIjZg-lTH0AAD',
 	'settings' => [
 		'mode' => 'sandbox',
 		'http.ConnectionTimeOut' => 1000,
