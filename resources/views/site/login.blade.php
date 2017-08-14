47b1ce5bc4442b460407c9696706b9b166749465
Marcelo Braga <mcbraga@hotmail.com>
2017-08-14 10:19:37 -0300

    Correct email and password label layout on login page.

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -39,7 +39,7 @@
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                         <br><br>                            
                         <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4 ">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <label for="email" class="col-md-4 control-label form-home">E-MAIL</label>
                             <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                             @if ($errors->has('email'))
                                 <span class="help-block">
@@ -50,7 +50,7 @@
                     </div>
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">                            
                         <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4 ">
-                            <label for="password" class="col-md-4 control-label">Senha</label>
+                            <label for="password" class="col-md-4 control-label form-home">SENHA</label>
                             <input id="password" type="password" class="form-control" name="password" required>
                             @if ($errors->has('password'))
                                 <span class="help-block">
