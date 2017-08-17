3c1a25239d9d1ec2047d99ae81ea69a6b3dbb2f3
Marcelo Braga <mcbraga@hotmail.com>
2017-08-17 01:14:51 +0000

    Configure contato@agradecaaqui.site account to send system's emails. Set Paypal's settings for sandbox and live environments.

--- config/paypal.php
+++ config/paypal.php
@@ -1,10 +1,15 @@
 <?php
 
 return [
-	'client_id' =>'AQkysL0A7hcLpfsuLRmjn9ecaRVs4K-ZFWBjm9kZbzowjQ2twenTdrQYb_AbpDBHhrHwDypcoT9q5VnZ',
-	'secret' => 'EFJoxsKnmN-knufnPWm6eC-S-WAAqFyRnml3LkKtgvMZQ00VzRFYAHZjhFGNzdytteY7xudzw-OsNkE4',
+	// Sandbox
+	'client_id' =>'AVc4QxpdLph6UjkfvL3q15jF1IBxQBd-ZlGAFbTwhw1NIvDuRYkRwgMw9UXpEQg7I0owNNZn6UEeycZP',
+	'secret' => 'EN7qf2mvI_f8isRvCuiYVVg8hRa5w1KXoDVPEXOYclWYZ0dwAtELdmRgs28sS_dFDpPPIjZg-lTH0AAD',
+	// Production (Live)
+	//'client_id' =>'AQkysL0A7hcLpfsuLRmjn9ecaRVs4K-ZFWBjm9kZbzowjQ2twenTdrQYb_AbpDBHhrHwDypcoT9q5VnZ',
+	//'secret' => 'EFJoxsKnmN-knufnPWm6eC-S-WAAqFyRnml3LkKtgvMZQ00VzRFYAHZjhFGNzdytteY7xudzw-OsNkE4',
 	'settings' => [
-		'mode' => 'live', // Available options: 'sandbox' or 'live'.
+		'mode' => 'sandbox', // Available options: 'sandbox' or 'live'.
+		//'mode' => 'live', // Available options: 'sandbox' or 'live'.
 		'http.ConnectionTimeOut' => 1000,
 		'log.LogEnabled' => true,
 		'log.FileName' => storage_path() . '/logs/paypal.log',
