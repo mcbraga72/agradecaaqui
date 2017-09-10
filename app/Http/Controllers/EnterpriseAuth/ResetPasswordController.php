e48639284be9151fec83979786583cc3edc804fb
Marcelo Braga <mcbraga@hotmail.com>
2017-09-09 23:28:34 -0300

    Change url that enterprise users and admins are redirected after resetting their passwords.

--- app/Http/Controllers/EnterpriseAuth/ResetPasswordController.php
+++ app/Http/Controllers/EnterpriseAuth/ResetPasswordController.php
@@ -28,7 +28,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/empresa/entrar';
+    protected $redirectTo = '/empresa/painel';
 
     /**
      * Create a new controller instance.
